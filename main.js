e6a2fe287eab09431080147892fac00ab4a3839e
Dilshod <dilshodakromboyev@gmail.com>
2024-10-14 14:36:56 +0500

    finish

---
+++ main.js
@@ -0,0 +1,23 @@
+
+let ism = prompt("Ismingizni kiriting!");
+let familya = prompt("Familyangizni kiriting!");
+let yoshi = prompt("Yoshingizni kiriting!");
+let yashashJoyi = prompt("Manzilingizni kiriting!");
+let telefonNomer = prompt("Telefon nomerni kiriting!");
+
+console.log(`
+     Sizning ismigiz: ${ism},
+     Sizning familyangiz: ${familya},
+     Sizning yoshingiz: ${yoshi},
+     Sizning yashash joyingiz :${yashashJoyi},
+     Sizning telefon raqamingiz: ${telefonNomer},
+    `);
+    
+    alert(`
+    Sizning ismigiz: ${ism},
+    Sizning familyangiz: ${familya},
+    Sizning yoshingiz: ${yoshi},
+    Sizning yashash joyingiz :${yashashJoyi},
+    Sizning telefon raqamingiz: ${telefonNomer},
+  
+    `); 
